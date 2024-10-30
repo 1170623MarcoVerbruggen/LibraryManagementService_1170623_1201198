031c0f88cfc1d4928cbcc52d59d41a53ee38ecb1
MarcoVerbruggen <1170623@isep.ipp.pt>
2024-10-30 16:30:14 +0000

    test: add test classes for ID Generation
    added test classes for ID Generation feature
    
    Resolves Issue #20

---
+++ src/test/java/pt/psoft/g1/psoftg1/idgeneration/IDGeneration24HexTest.java
@@ -0,0 +1,62 @@
+package pt.psoft.g1.psoftg1.idgeneration;
+
+import org.junit.jupiter.api.Test;
+import pt.psoft.g1.psoftg1.idggeneration.IDGeneration;
+import pt.psoft.g1.psoftg1.idggeneration.IDGeneration24Hex;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IDGeneration24HexTest {
+
+    private static final int CHAR_LIMIT = 20;
+
+    @Test
+    void testGenerateIDConformsToFormat() {
+
+        //arrange
+        IDGeneration gen = new IDGeneration24Hex();
+        String testISBN0 = "978-3-16-148410-0";
+
+        //act
+        String id0 = gen.generateID(testISBN0);
+
+        //assert
+        assertTrue(id0.matches("^[a-zA-Z0-9]{"+CHAR_LIMIT+"}$"));
+
+    }
+
+    @Test
+    void testGenerateIDDifferentResultsFromDifferentBusinessID() {
+
+        //arrange
+        IDGeneration gen = new IDGeneration24Hex();
+        String testISBN0 = "978-3-16-148410-0";
+        String testISBN1 = "978-3-16-148410-1";
+
+        //act
+        String id0 = gen.generateID(testISBN0);
+        String id1 = gen.generateID(testISBN1);
+
+        //assert
+        assertNotEquals(id0, id1);
+
+    }
+
+    @Test
+    void testGenerateIDSameResultsFromSameBusinessID() {
+
+        //arrange
+        IDGeneration gen = new IDGeneration24Hex();
+        String testISBN0 = "978-3-16-148410-0";
+        String testISBN1 = "978-3-16-148410-0";
+
+        //act
+        String id0 = gen.generateID(testISBN0);
+        String id1 = gen.generateID(testISBN1);
+
+        //assert
+        assertNotEquals(id0, id1);
+
+    }
+
+}
